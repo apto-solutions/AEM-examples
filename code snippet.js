52b3384306cb8f3e601a1d3a8a7eed2902c4d19e
steven.radford <steven.radford@aptosolutions.co.uk>
2020-02-17 14:01:18 +0000

    Add code snippet of AEM example code.

---
+++ code snippet.js
@@ -0,0 +1,20 @@
+/**
+ * Example code snippet
+ * 
+ */
+ 
+ // The following is an example fo the code we might deliver
+ 
+ ...
+ 
+var i = 0;
+i++;
+
+console.println(i);
+
+...
+
+
+/**
+ * this is just an example
+ */
